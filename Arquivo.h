2e98840de32dca428923a860e7ce9fb14cd43474
RoyCal <vito.elias9@gmail.com>
2022-04-18 11:31:14 -0300

    pequeno ajuste

--- Arquivo.h
+++ Arquivo.h
@@ -1,15 +1,13 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <string>
 
 using namespace std;
 
 class Arquivo{
     public:
         void criaArquivo();
-        
+
         void imprimeArquivo();
 
         fstream arquivo;
